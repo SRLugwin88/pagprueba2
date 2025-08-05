25141a309900f4fd5ca24bc6489f318cf6c56d22
SRLugwin88 <exequiel20039@gmail.com>
2025-08-04 22:07:31 -0300

    cambios8

--- FrontEnd/js/usuarios.js
+++ FrontEnd/js/usuarios.js
@@ -1,5 +1,5 @@
 // Configuración de la API
-const API_BASE_URL = 'VITE_API_BASE_URL';
+const API_BASE_URL = 'https://pagprueba2-production-b22b.up.railway.app';
 
 // Variables globales
 let usuariosData = [];
